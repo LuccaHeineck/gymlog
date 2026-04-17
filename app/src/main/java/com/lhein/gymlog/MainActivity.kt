776e36f911b3c7e300c403404f6675202500bc6c
LuccaHeineck <luccacheineck@gmail.com>
2026-04-16 22:51:29 -0300

    feat: create first interface components and binding

--- app/src/main/java/com/lhein/gymlog/MainActivity.kt
+++ app/src/main/java/com/lhein/gymlog/MainActivity.kt
@@ -5,16 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayout
+import com.lhein.gymlog.adapter.WorkoutAdapter
+import com.lhein.gymlog.databinding.ActivityMainBinding
+import com.lhein.gymlog.model.Workout
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private var listWorkouts = emptyList<Workout>()
+    private val workoutAdapter: WorkoutAdapter? = null;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        //TODO filtrar apenas dias que ja tem treinos
+        val diasSemana = listOf("Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom")
+        diasSemana.forEach { dia ->
+            binding.tabLayoutDays.addTab(binding.tabLayoutDays.newTab().setText(dia))
         }
+
+        binding.tabLayoutDays.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val diaSelecionado = tab?.text.toString()
+                // Chame sua função de filtro aqui:
+                // viewModel.filtrarTreinosPorDia(diaSelecionado)
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
     }
 }
---
+++ app/src/main/java/com/lhein/gymlog/adapter/WorkoutAdapter.kt
@@ -0,0 +1,30 @@
+package com.lhein.gymlog.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lhein.gymlog.databinding.ItemWorkoutBinding
+
+class WorkoutAdapter() : RecyclerView.Adapter<WorkoutAdapter.WorkoutViewHolder>() {
+
+    inner class WorkoutViewHolder(itemBinding: ItemWorkoutBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+        //TODO("Implementar")
+    }
+
+    override fun onCreateViewHolder(
+        p0: ViewGroup,
+        p1: Int
+    ): WorkoutViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(
+        p0: WorkoutViewHolder,
+        p1: Int
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
